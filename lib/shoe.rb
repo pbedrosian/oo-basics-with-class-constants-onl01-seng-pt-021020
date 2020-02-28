823811eebbab04567018edb45302fdf5f237dc82
Peter Bedrosian <peter_bedrosian@yahoo.com>
2020-02-28 00:06:19 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -3,12 +3,14 @@ class Shoe
   attr_reader :brand
 
   BRANDS = []
-
+  
   def initialize(brand)
-    @brand = brand
+  @brand = brand
+  if BRANDS.include?(brand)
+  else
     BRANDS << brand
   end
-
+end
   def cobble
     self.condition = "new"
     puts "Your shoe is as good as new!"
